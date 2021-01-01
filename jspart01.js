14b8aaa5f71a0761d31256d6a091b5a69da33307
badhon86 <badhon35-2092@diu.edu.bd>
2021-01-02 01:15:57 +0600

    nothing

--- jspart01.js
+++ jspart01.js
@@ -1,7 +1,7 @@
 
 document.getElementById("demo").innerHTML = "My First JavaScript";
 document.getElementById("demo2").innerHTML = 5 + 6;
-document.write(100+111);
+
 var x, y, z;  // Statement 1
 x = 5;        // Statement 2
 y = 6;        // Statement 3
@@ -9,3 +9,15 @@ z = x + y;    // Statement 4
 
 document.getElementById("demo3").innerHTML =
 "The value of z is " + z + ".";  
+
+
+function myfunction() {
+    document.getElementById("demo4").innerHTML = "ball";
+      
+}
+
+var lastname,firstname;
+lastname="badhon";
+firstname="sarkar";
+document.getElementById("demo5").innerHTML=firstname;
+document.getElementById("demo5").innerHTML=firstname+" "+lastname;
