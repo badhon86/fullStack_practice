35db1e1c969948c06d8b938717956d42bdb9e865
badhon86 <badhon35-2092@diu.edu.bd>
2021-01-01 23:17:37 +0600

    nothing

--- jspart01.js
+++ jspart01.js
@@ -1,2 +1,11 @@
 
 document.getElementById("demo").innerHTML = "My First JavaScript";
+document.getElementById("demo2").innerHTML = 5 + 6;
+document.write(100+111);
+var x, y, z;  // Statement 1
+x = 5;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
+
+document.getElementById("demo3").innerHTML =
+"The value of z is " + z + ".";  
